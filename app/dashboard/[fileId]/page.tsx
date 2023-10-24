15bb2d77945c0c90a4afb2c4755adda840e000ca
thibaud-g <thibaud.gippet@gmail.com>
2023-10-24 23:00:03 +0200

    fix: add subscription plan to chat wrapper prosps

--- app/dashboard/[fileId]/page.tsx
+++ app/dashboard/[fileId]/page.tsx
@@ -1,48 +1,52 @@
 import ChatWrapper from "@/components/chat/chat-wrapper";
 import PdfRenderer from "@/components/pdf-renderer";
 import { db } from "@/db";
+import { getUserSubscriptionPlan } from "@/lib/stripe";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
-import React from "react";
 
 interface PageProps {
   params: {
-    fileId: string;
+    fileid: string;
   };
 }
 
-const page = async ({ params }: PageProps) => {
-  const { fileId } = params;
+const Page = async ({ params }: PageProps) => {
+  const { fileid } = params;
 
   const { getUser } = getKindeServerSession();
   const user = getUser();
 
-  if (!user) redirect(`/auth-callback?origin=dashboard/${fileId}`);
+  if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileid}`);
 
   const file = await db.file.findFirst({
     where: {
-      id: fileId,
+      id: fileid,
       userId: user.id,
     },
   });
 
-  if (!file) return notFound();
+  if (!file) notFound();
+
+  const plan = await getUserSubscriptionPlan();
 
   return (
     <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
       <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
-        {/* left side */}
+        {/* Left sidebar & main wrapper */}
         <div className="flex-1 xl:flex">
           <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            {/* Main area */}
             <PdfRenderer url={file.url} />
           </div>
         </div>
+
         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-          <ChatWrapper fileId={file.id} />
+          <ChatWrapper isSubscribed={plan.isSubscribed} fileId={file.id} />
         </div>
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
